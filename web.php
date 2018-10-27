67d7883c28c82c9eeade36446bd38fdd6f435cf6
wzc <515819056@qq.com>
2018-10-27 15:22:04 +0800

    提交验证码和models存放

--- web.php
+++ web.php
@@ -33,5 +33,3 @@ Auth::routes();
 //Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 //Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //Route::get('/home', 'HomeController@index')->name('home');
-
-
