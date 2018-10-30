e2b024b4fc44629309dab42755bc43ee361ad6c8
wzc <515819056@qq.com>
2018-10-31 00:57:13 +0800

    List topics created by user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -38,7 +38,12 @@
             {{-- 用户发布的内容 --}}
             <div class="panel panel-default">
                 <div class="panel-body">
-                    暂无数据 ~_~
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#">Ta 的话题</a></li>
+                        <li><a href="#">Ta 的回复</a></li>
+                    </ul>
+                    {{--recent // 按照创建时间排序  topics的构造器--}}
+                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
                 </div>
             </div>
 
